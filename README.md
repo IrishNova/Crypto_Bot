ff29cd8971ec32fa2d973bf49af50dd9e126cb7c
Ryan Sweeny <64923468+IrishNova@users.noreply.github.com>
2022-01-03 13:29:42 -0600

    See ReadMe
    You're looking at a program that's largely a backtester. The code can be quickly integrated to perform automated trades when connected with an API and will use the same logic as the back tester.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Crypto_Bot
- An expiremental trading bot for crypto currencies
+ An experimental trading bot for cryptocurrencies
+ 
+This bot isn't intended to be used for live trading. Rather, it's an experimental program designed to teach me, the crater, how trade automation works and what an automated program looks like. It also has helped me hone my coding skills. 
+
+Some big takeaways are:
+• Crypto markets are incredibly chaotic. That is, the random walk is particularly random in the crypto space. Which, obviously, makes a lot of sense and the coins are somewhere between penny stocks and ponzi schemes in the scope of assets.
+• Bots are easy to make but will almost always loose money over the short run and, will always lose money over the long run.
---
+++ warehouse.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from pymongo import MongoClient
+import certifi
+
+
+class Daryl:
+    def __init__(self, coin):
+        self.coin = coin
+        self.raw_data = None
+        self.worked_data = None
+        self.matrix = None
+        self.data_retriever()
+        # self.data_calc()
+
+    def mongo_cluster(self):
+        """
+        Connects to MongoDB Atlas Cloud
+        cluster = connection to cluster on cloud
+        db = specific database within cluster, titled "crypto", though it contains all data for this project.
+        :return: variable db which is set up for entry of specific item selected by input
+        """
+        cluster = MongoClient(
+            "mongodb+srv://admin:pword123@beta.jpgpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            tlsCAFile=certifi.where())
+        db = cluster["coin_data"]
+        return db
+
+    def data_retriever(self):
+        """
+        Uses connection set up in mongo_cluster() function to access specific item's data, retrieves it and packages that
+        data as a DataFrame.
+        :param one: User input
+        :return: DataFrame of desired item
+        """
+        col = self.mongo_cluster()["{0}".format(self.coin)]
+        temp = []
+        for doc in col.find():
+            temp.append(doc)
+        export = pd.DataFrame(temp, columns=["Date", "Price", 'Market Cap', 'Volume', 'market cap dominance'])
+        export = export.set_index("Date")
+        self.raw_data = export
+        return
